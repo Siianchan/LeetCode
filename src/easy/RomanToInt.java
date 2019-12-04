7ee9274deb276dbe2c10b26d284268bd91218b14
Ai_Sama <79479334@qq.com>
2019-12-04 20:58:22 +0800

    2019-12-04 RomanToInt

--- src/easy/RomanToInt.java
+++ src/easy/RomanToInt.java
@@ -26,7 +26,7 @@ public class RomanToInt {
      * 给定一个罗马数字，将其转换成整数。输入确保在 1 到 3999 的范围内。
      */
     public static void main(String args[]) {
-            System.out.println(romanToInt("LVIII"));
+            System.out.println(romanToInt("MCMXCIV"));
     }
 
     public  static  int romanToInt(String s) {
