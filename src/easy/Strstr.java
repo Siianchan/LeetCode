f2356419d530e930e3d4e99c61edac998fd63984
Ai_Sama <79479334@qq.com>
2019-12-20 19:49:45 +0800

    2019-12-20 Strstr

---
+++ src/easy/Strstr.java
@@ -0,0 +1,27 @@
+package easy;
+
+/**
+ * @ProjectName: LeetCode
+ * @Author: ZhangXiangQiang
+ * @Create: 2019-12-20 19:47
+ * @Description:
+ */
+public class Strstr {
+    /**
+     * 实现 strStr()
+     * 实现 strStr() 函数。
+     * 给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+     * 示例 1:
+     * 输入: haystack = "hello", needle = "ll"
+     * 输出: 2
+     * 示例 2:
+     * 输入: haystack = "aaaaa", needle = "bba"
+     * 输出: -1
+     */
+    public static void main(String args[]) {
+
+    }
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
