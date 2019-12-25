7555d8e5bb8bfd7da34afcf83ec9234953bf2668
Ai_Sama <79479334@qq.com>
2019-12-25 15:17:46 +0800

    2019-12-25 generateParenthesis

--- src/medium/GenerateParenthesis.java
+++ src/medium/GenerateParenthesis.java
@@ -28,7 +28,7 @@ public class GenerateParenthesis {
     }
 
     public static List<String> generateParenthesis(int n) {
-
+        //运用了回溯递归法
         ArrayList arrayList = new ArrayList();
         generate("", n, n, arrayList);
 //        System.out.println(arrayList);
@@ -36,14 +36,17 @@ public class GenerateParenthesis {
     }
 
     public static void generate(String str, int left, int right, List list) {
+        //当左括号和右括号都添加完毕的时候，在集合中添加该条记录，并终止递归
         if (left == 0 && right == 0) {
             list.add(str);
             return;
         }
         if (left > 0) {
+            //当左括号还没添加完成时，添加一个左括号，进入下一层递归
             generate(str + "(", left - 1, right, list);
         }
         if (left < right) {
+            //当左括号多余右括号时，添加一个右括号，进入下一层递归
             generate(str + ")", left, right - 1, list);
         }
     }
