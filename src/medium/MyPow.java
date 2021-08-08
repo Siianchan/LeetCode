6d000f833bc21ce6b86b8b94d8b542a495f5bdb5
79479334 <79479334@qq.com>
2021-08-08 22:13:56 +0800

    2021-8-08 MyPow

---
+++ src/medium/MyPow.java
@@ -0,0 +1,24 @@
+package medium;
+
+/**
+ * @ProjectName: LeetCode
+ * @Author: ZhangXiangQiang
+ * @Create: 2021/8/8 22:12
+ * @Description:
+ */
+public class MyPow {
+    /**
+     * 实现 pow(x, n) ，即计算 x 的 n 次幂函数（即，xn）。
+     * 示例 1：
+     * 输入：x = 2.00000, n = 10
+     * 输出：1024.00000
+     */
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+    public double myPow(double x, int n) {
+        return Math.pow(x, n);
+    }
+}
