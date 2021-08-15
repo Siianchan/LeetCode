b18024139ed8f3e85cc70a409d8de28e773828d6
79479334 <79479334@qq.com>
2021-08-15 16:53:00 +0800

    2021-8-15 MySqrt

---
+++ src/easy/MySqrt.java
@@ -0,0 +1,16 @@
+package easy;
+
+/**
+ * @ProjectName: LeetCode
+ * @Author: ZhangXiangQiang
+ * @Create: 2021/8/15 16:49
+ * @Description:
+ */
+public class MySqrt {
+    //平方根
+    public static void main(String[] args){
+    }
+    public int mySqrt(int x) {
+        return (int)Math.sqrt(x);
+    }
+}
