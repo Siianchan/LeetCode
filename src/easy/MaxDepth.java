6ed45ac41dec715fd43cd4a63eee06bb0919f41c
79479334 <79479334@qq.com>
2021-09-03 20:07:29 +0800

    2021-9-03 MaxDepth

--- src/easy/MaxDepth.java
+++ src/easy/MaxDepth.java
@@ -7,7 +7,7 @@ package easy;
  * @Description:
  */
 public class MaxDepth {
-    
+
     /**
      * 给定一个二叉树，找出其最大深度。
      * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
@@ -18,7 +18,6 @@ public class MaxDepth {
      */
 
     int num = 0;
-
     public static void main(String[] args) {
         TreeNode treeNode = new TreeNode(1);
         TreeNode treeNode2 = new TreeNode(2);
