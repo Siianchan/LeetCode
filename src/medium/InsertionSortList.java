be3d7e22682b91fc79d7b5c9d9c1ac57a844f52a
79479334 <79479334@qq.com>
2021-10-02 18:23:05 +0800

    2021-10-02 InsertionSortList

---
+++ src/medium/InsertionSortList.java
@@ -0,0 +1,36 @@
+package medium;
+
+/**
+ * @ProjectName: LeetCode
+ * @Author: ZhangXiangQiang
+ * @Create: 2021/10/2 17:56
+ * @Description:
+ */
+public class InsertionSortList {
+    public static void main(String[] args){
+
+    }
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode lastSorted = head, curr = head.next;
+        while (curr != null) {
+            if (lastSorted.val <= curr.val) {
+                lastSorted = lastSorted.next;
+            } else {
+                ListNode prev = dummyHead;
+                while (prev.next.val <= curr.val) {
+                    prev = prev.next;
+                }
+                lastSorted.next = curr.next;
+                curr.next = prev.next;
+                prev.next = curr;
+            }
+            curr = lastSorted.next;
+        }
+        return dummyHead.next;
+    }
+}
