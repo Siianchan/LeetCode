b069d8fb4ed0817f697dd288c1c30c39ffc84ad3
79479334 <79479334@qq.com>
2021-10-19 20:14:10 +0800

    2021-10-19 TwoSum

--- src/easy/TwoSum.java
+++ src/easy/TwoSum.java
@@ -10,6 +10,7 @@ import java.util.Set;
  * @Description:
  */
 public class TwoSum {
+    
     /**
      * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
      * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
@@ -19,7 +20,7 @@ public class TwoSum {
      * 所以返回[0,1]
      */
     public static void main(String[] args) {
-        twoSum(new int[]{2, 7, 11, 15}, 9);
+        twoSum2(new int[]{2, 7, 11, 15}, 9);
     }
 
     public static int[] twoSum(int[] nums, int target) {
@@ -39,7 +40,7 @@ public class TwoSum {
         return answer;
     }
 
-    public int[] twoSum2(int[] nums, int target) {
+    public  static  int[] twoSum2(int[] nums, int target) {
         int[] answer = new int[2];
         HashMap<Integer, Integer> hashMap = new HashMap();
         HashMap<Integer, Integer> hashMap2 = new HashMap();
@@ -53,6 +54,7 @@ public class TwoSum {
         Set<Integer> set = hashMap.keySet();
         for (Integer s : set
         ) {
+            System.out.println(s);
             int arrA = hashMap.get(s);
             int a = target - s;
             Integer arrB;
