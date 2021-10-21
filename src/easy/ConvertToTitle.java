000d4c1feb3be233c5ac5d353169d684248a931d
79479334 <79479334@qq.com>
2021-10-21 17:26:38 +0800

    2021-10-21 ConvertToTitle

---
+++ src/easy/ConvertToTitle.java
@@ -0,0 +1,49 @@
+package easy;
+
+/**
+ * @ProjectName: LeetCode
+ * @Author: ZhangXiangQiang
+ * @Create: 2021/10/21 16:44
+ * @Description:
+ */
+public class ConvertToTitle {
+
+    /**
+     * 给你一个整数 columnNumber ，返回它在 Excel 表中相对应的列名称。
+     * 例如：
+     * A -> 1
+     * B -> 2
+     * C -> 3
+     * ...
+     * Z -> 26
+     * AA -> 27
+     * AB -> 28
+     * ...
+     */
+
+
+    public static void main(String[] args) {
+        ConvertToTitle convertToTitle = new ConvertToTitle();
+        System.out.println(convertToTitle.convertToTitle(2147483647));
+    }
+
+    public String convertToTitle(int columnNumber) {
+        StringBuilder stringBuilder = new StringBuilder();
+        while (columnNumber > 0) {
+            int yu = columnNumber % 26;
+            if (yu != 0) {
+                char c = (char) ('A' + yu - 1);
+                stringBuilder.insert(0, String.valueOf(c));
+            } else {
+                stringBuilder.insert(0, "Z");
+                if (columnNumber > (Math.pow(26, stringBuilder.length()))) {
+                    columnNumber -= Math.pow(26, stringBuilder.length());
+                } else {
+                    return stringBuilder.toString();
+                }
+            }
+            columnNumber /= 26;
+        }
+        return stringBuilder.toString();
+    }
+}
