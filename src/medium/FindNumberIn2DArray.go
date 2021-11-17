b02568e05ddacd994d84b4bbc3e4ce3a1cb0295e
79479334 <79479334@qq.com>
2021-11-17 13:57:09 +0800

    2021-11-17  findNumberIn2DArray golang

---
+++ src/medium/FindNumberIn2DArray.go
@@ -0,0 +1,35 @@
+package main
+
+/*在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+请完成一个高效的函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+现有矩阵 matrix 如下：
+[
+[1,   4,  7, 11, 15],
+[2,   5,  8, 12, 19],
+[3,   6,  9, 16, 22],
+[10, 13, 14, 17, 24],
+[18, 21, 23, 26, 30]
+]
+给定 target = 5，返回 true。
+给定 target = 20，返回 false。*/
+
+func main() {
+
+}
+func findNumberIn2DArray(matrix [][]int, target int) bool {
+	if matrix == nil || len(matrix) == 0 || len(matrix[0]) == 0 {
+		return false
+	}
+	x := 0
+	y := len(matrix[0]) - 1
+	for x < len(matrix) && y >= 0 { //从右上角开始查找，当大于时，往左移动一格，当小于时，往下移动一格
+		if matrix[x][y] == target {
+			return true
+		} else if matrix[x][y] > target {
+			y--
+		} else {
+			x++
+		}
+	}
+	return false
+}
