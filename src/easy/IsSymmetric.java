db5e07205734d7dd227e0b60c46230991d667fa9
79479334 <79479334@qq.com>
2021-12-10 14:12:17 +0800

    2021-12-10 isSymmetric java

--- src/easy/IsSymmetric.java
+++ src/easy/IsSymmetric.java
@@ -10,21 +10,18 @@ package easy;
 public class IsSymmetric {
 //    给定一个二叉树，检查它是否是镜像对称的。
 //    例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
-    static StringBuilder stringBuilder = new StringBuilder();
 
-    public static void main(String[] args) {
-        TreeNode treeNode = new TreeNode(1);
-        TreeNode fir = treeNode;
-        TreeNode treeNode2 = new TreeNode(2);
-        TreeNode treeNode3 = new TreeNode(2);
-        treeNode.left = treeNode2;
-        treeNode.right = treeNode3;
-        IsSymmetric symmetric = new IsSymmetric();
+    public boolean isSymmetric(TreeNode root) {
+        if (root != null) {
+            return isMirror(root.left, root.right);
+        }
+        return true;
     }
 
     public boolean isMirror(TreeNode t1, TreeNode t2) {
-        if (t1 == null && t2 == null) return true;
-        if (t1 == null || t2 == null) return false;
+        if (t1 == null && t2 == null) return true; //当两个节点都为空时，返回true
+        if (t1 == null || t2 == null) return false;//当有一方节点为空时，返回false
+        //判断当前值是否相等，然后递归判断t1左树和t2右树
         return (t1.val == t2.val)
                 && isMirror(t1.right, t2.left)
                 && isMirror(t1.left, t2.right);
