0bd8bc13a8d16c1a34c4f0e89b200aa385df931c
79479334 <79479334@qq.com>
2021-12-15 21:11:51 +0800

    2021-12-15 CopyRandomList java

--- src/medium/CopyRandomList.java
+++ src/medium/CopyRandomList.java
@@ -11,6 +11,14 @@ import java.util.Optional;
  * @Description:
  */
 
+/**
+ * 请实现 copyRandomList 函数，复制一个复杂链表。在复杂链表中，
+ * 每个节点除了有一个 next 指针指向下一个节点，还有一个 random 指针指向链表中的任意节点或者 null。
+ * 示例 1：
+ * 输入：head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+ * 输出：[[7,null],[13,0],[11,4],[10,2],[1,0]]
+ */
+
 class NodeR {
     int val;
     NodeR next;
