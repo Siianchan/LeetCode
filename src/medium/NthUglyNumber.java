78bed68a3a3aacf40ab7ebc290ffa04bce192f1f
79479334 <79479334@qq.com>
2021-12-22 16:23:29 +0800

    2021-12-22 NthUglyNumber java

--- src/medium/NthUglyNumber.java
+++ src/medium/NthUglyNumber.java
@@ -1,6 +1,5 @@
 package medium;
 
-import sun.nio.cs.ext.MacHebrew;
 
 /**
  * @ProjectName: LeetCode
@@ -8,6 +7,16 @@ import sun.nio.cs.ext.MacHebrew;
  * @Create: 2021/12/22 14:58
  * @Description:
  */
+
+
+/**
+ * 我们把只包含质因子 2、3 和 5 的数称作丑数（Ugly Number）。求按从小到大的顺序的第 n 个丑数。
+ * 示例:
+ * 输入: n = 10
+ * 输出: 12
+ * 解释: 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 是前 10 个丑数。
+ */
+
 public class NthUglyNumber {
     public static void main(String[] args) {
         System.out.println(nthUglyNumber(13));
