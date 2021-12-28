b0aca4b6aa124e930b5c026d5b31dff20b35e24e
79479334 <79479334@qq.com>
2021-12-28 15:21:33 +0800

    2021-12-28 FindContinuousSequence java

--- src/easy/FindContinuousSequence.java
+++ src/easy/FindContinuousSequence.java
@@ -10,6 +10,18 @@ import java.util.List;
  * @Description:
  */
 public class FindContinuousSequence {
+
+    /**
+     * 输入一个正整数 target ，输出所有和为 target 的连续正整数序列（至少含有两个数）。
+     * 序列内的数字由小到大排列，不同序列按照首个数字从小到大排列。
+     * 示例 1：
+     * 输入：target = 9
+     * 输出：[[2,3,4],[4,5]]
+     * 示例 2：
+     * 输入：target = 15
+     * 输出：[[1,2,3,4,5],[4,5,6],[7,8]]
+     */
+
     public static void main(String[] args) {
 
     }
