03fe9de8a2e986da34c381628988061c61374d99
79479334 <79479334@qq.com>
2021-12-28 15:20:50 +0800

    2021-12-28 findContinuousSequence java

---
+++ src/easy/FindContinuousSequence.java
@@ -0,0 +1,46 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ProjectName: LeetCode
+ * @Author: ZhangXiangQiang
+ * @Create: 2021/12/28 15:20
+ * @Description:
+ */
+public class FindContinuousSequence {
+    public static void main(String[] args) {
+
+    }
+
+    public int[][] findContinuousSequence(int target) {
+        int i = 1; // 滑动窗口的左边界
+        int j = 1; // 滑动窗口的右边界
+        int sum = 0; // 滑动窗口中数字的和
+        List<int[]> res = new ArrayList<>();
+
+        while (i <= target / 2) {
+            if (sum < target) {
+                // 右边界向右移动
+                sum += j;
+                j++;
+            } else if (sum > target) {
+                // 左边界向右移动
+                sum -= i;
+                i++;
+            } else {
+                // 记录结果
+                int[] arr = new int[j - i];
+                for (int k = i; k < j; k++) {
+                    arr[k - i] = k;
+                }
+                res.add(arr);
+                // 左边界向右移动
+                sum -= i;
+                i++;
+            }
+        }
+        return res.toArray(new int[res.size()][]);
+    }
+}
