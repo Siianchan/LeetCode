0043877c2a88d53f00fb759a72a6be4e659bbc38
79479334 <79479334@qq.com>
2022-01-02 14:50:45 +0800

    2022-01-02 reverseWords java

--- src/easy/ReverseWords.java
+++ src/easy/ReverseWords.java
@@ -7,6 +7,14 @@ package easy;
  * @Description:
  */
 public class ReverseWords {
+    /**
+     * 输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。
+     * 为简单起见，标点符号和普通字母一样处理。例如输入字符串"I am a student. "，则输出"student. a am I"。
+     * 示例 1：
+     * 输入: "the sky is blue"
+     * 输出: "blue is sky the"
+     */
+
     public static void main(String[] args) {
 
     }
