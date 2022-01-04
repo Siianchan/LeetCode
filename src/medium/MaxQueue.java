bb2a14809cff8fb63b3d60c3bf32bfd72d661be8
79479334 <79479334@qq.com>
2022-01-04 14:51:54 +0800

    2022-01-04 MaxQueue java

--- src/medium/MaxQueue.java
+++ src/medium/MaxQueue.java
@@ -1,7 +1,6 @@
 package medium;
 
 import java.util.ArrayDeque;
-import java.util.Queue;
 
 /**
  * @ProjectName: LeetCode
@@ -10,6 +9,18 @@ import java.util.Queue;
  * @Description:
  */
 public class MaxQueue {
+
+    /**
+     * 请定义一个队列并实现函数 max_value 得到队列里的最大值，
+     * 要求函数max_value、push_back 和 pop_front 的均摊时间复杂度都是O(1)。
+     * 若队列为空，pop_front 和 max_value 需要返回 -1
+     * 示例 1：
+     * 输入:
+     * ["MaxQueue","push_back","push_back","max_value","pop_front","max_value"]
+     * [[],[1],[2],[],[],[]]
+     * 输出: [null,null,null,2,1,2]
+     */
+
 
     ArrayDeque<Integer> queue = new ArrayDeque();
     ArrayDeque<Integer> maxQueue = new ArrayDeque();
