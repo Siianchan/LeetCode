616d628b31bda8860c6ecc45a4b9f4bf80712d45
79479334 <79479334@qq.com>
2022-01-05 15:47:12 +0800

    2022-01-05 IsStraight java

--- src/easy/IsStraight.java
+++ src/easy/IsStraight.java
@@ -9,6 +9,13 @@ import java.util.Arrays;
  * @Description:
  */
 public class IsStraight {
+    /**
+     * 从若干副扑克牌中随机抽 5 张牌，判断是不是一个顺子，即这5张牌是不是连续的。
+     * 2～10为数字本身，A为1，J为11，Q为12，K为13，而大、小王为 0 ，可以看成任意数字。A 不能视为 14。
+     * 示例 1:
+     * 输入: [1,2,3,4,5]
+     * 输出: True
+     */
 
     public static void main(String[] args) {
 
