c581fa7395726e56332217f8ffeb26284f4bd45a
79479334 <79479334@qq.com>
2022-01-07 16:10:24 +0800

    2022-01-07 maxProfit java

--- src/medium/MaxProfit.java
+++ src/medium/MaxProfit.java
@@ -7,17 +7,21 @@ package medium;
  * @Description:
  */
 public class MaxProfit {
-
+    /**
+     * 假设把某股票的价格按照时间先后顺序存储在数组中，
+     * 请问买卖该股票一次可能获得的最大利润是多少？
+     * 示例 1:
+     * 输入: [7,1,5,3,6,4]
+     * 输出: 5
+     * 解释: 在第 2 天（股票价格 = 1）的时候买入，
+     * 在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+     * 注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+     */
     public int maxProfit(int[] prices) {
-        int minValue = Integer.MAX_VALUE;
-        int maxValue = 0;
+        int minValue = Integer.MAX_VALUE, maxValue = 0;
         for (int x = 0; x < prices.length; x++) {
-            if (prices[x] < minValue) {
-                minValue = prices[x];
-            }
-            if (prices[x] - minValue > maxValue) {
-                maxValue = prices[x] - minValue;
-            }
+            minValue = Math.min(prices[x], minValue);
+            maxValue = Math.max(maxValue, prices[x] - minValue);
         }
         return maxValue;
     }
