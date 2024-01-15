e5e5947dc0ea4a29f5387f088f051082a5cda9b4
Siianchan <79479334@qq.com>
2024-01-15 09:51:33 +0800

    deleteDuplicates2

--- src/easy/DeleteDuplicates.java
+++ src/easy/DeleteDuplicates.java
@@ -12,7 +12,6 @@ public class DeleteDuplicates {
     }
 
     public ListNode deleteDuplicates(ListNode head) {
-        //111
         if (head == null) {
             return head;
         }
@@ -28,5 +27,26 @@ public class DeleteDuplicates {
             }
         }
         return first;
+    }
+
+    public ListNode deleteDuplicates2(ListNode head) {
+        // 给定一个已排序的链表的头 head ， 删除原始链表中所有重复数字的节点，只留下不同的数字 。返回 已排序的链表 。
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode pre = new ListNode(Integer.MIN_VALUE);
+        pre.next = head;
+        ListNode first = pre;
+        while (pre.next != null && pre.next.next != null) {
+            if (pre.next.val != pre.next.next.val) {
+                pre = pre.next;
+            } else {
+                int x = pre.next.val;
+                while (pre.next != null && pre.next.val == x) {
+                    pre.next = pre.next.next;
+                }
+            }
+        }
+        return first.next;
     }
 }
